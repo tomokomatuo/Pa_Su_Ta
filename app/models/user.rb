83d7eb4ad153681d154b79cf59f32a3050e32311
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-06-22 14:17:23 +0900

    heroku修正follower

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   
   validates :name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 255 }, 
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :address, presence: true
-  validates :phone_number, presence: true
+  validates :phone_number, presence: true, length: { minimum: 10 }
   validates :birthday, presence: true
   validates :gender, presence: true
   
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,9 +12,9 @@ RSpec.describe User, type: :model do
         expect(user).to be_valid
       end    
       it "is invalid without a name" do
-        user = User.new(name: nil)
+        user = User.new(email: '1@gmail.com', name: nil)
         user.valid?
-        expect(user.errors[:name]).to include("can't be blank")
+        expect(user.errors[:name]).to include("を入力してください")
       end
   it "is invalid with a duplicate title"
   it "is invalid with title is 101 or more characters"
