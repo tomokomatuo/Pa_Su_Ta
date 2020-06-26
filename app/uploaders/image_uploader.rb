494eb21e74dfa8b928ac2479313f310309ac015a
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-06-26 20:34:31 +0900

    heroku画像

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -18,7 +18,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   
-    "/default.png"
+    "./db/fixtures/default.png"
   end
 
   # Process files as they are uploaded:
