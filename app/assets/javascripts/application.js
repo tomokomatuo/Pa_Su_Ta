32244b99fafcd142797dd2cdf90439b0445485b5
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-06-27 12:09:11 +0900

    bootstrap削除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap/dist/js/bootstrap.min
 //= require jquery
 //= require activestorage
+//= require turbolinks
 //= require jquery/dist/jquery.js
 //= require rails-ujs
 //= require_tree .
